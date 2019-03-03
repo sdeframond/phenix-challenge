28ffe917ef5bc74a69ae80ace932290f3104e5ea
Samuel de Framond <samuel@framond.fr>
2019-03-03 18:37:32 +0100

    Read and write file

--- app.scala
+++ app.scala
@@ -1,3 +1,4 @@
 object Main extends App {
-  println("Hello, Lyon!")
+  val lines = scala.io.Source.fromFile("data/reference_prod-10f2f3e6-f728-41f3-b079-43b0aa758292_20170514.data")
+  lines.getLines() foreach println
 }
