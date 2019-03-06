18250e2db329ad81fabd1612b0e30c981af82cab
Samuel de Framond <samuel@framond.fr>
2019-03-06 20:30:36 +0100

    Read path to data from args

--- app.scala
+++ app.scala
@@ -2,7 +2,11 @@ import java.nio.file.{Files, Paths}
 import scala.collection.JavaConverters._
 
 object Main extends App {
-  val directory = "data"
+  if(args.length != 1) {
+    println("USAGE : phenix-challenge <path_to_data>")
+    System.exit(1)
+  }
+  val directory = args(0)
   val files = Files.walk(Paths.get(directory)).iterator().asScala
   for (f <- files.map(_.toFile).filter(_.isFile)) {
     val lines = scala.io.Source.fromFile(f.getPath)
